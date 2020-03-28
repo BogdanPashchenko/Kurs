b24cade2abd4bc9919df6e08c552ce18e1923d74
BogdanPaschenko <gitkhab@bk.ru>
2020-03-28 09:20:52 +0500

    new

---
+++ Kursovoy-master/AHardware/GpioPort.hpp
@@ -0,0 +1,27 @@
+#include "gpioaregisters.hpp" 
+#include "port.hpp"
+
+template<typename GPIOModule, std::uint32_t pinNum> //T = GpioModule
+class GpioPort
+{
+public:
+  void SetAlternate()
+  {
+    volatile auto value = GPIOModule::MODER::Get() ; //chitivaem znachenia moder
+    value&= ~(3 << (pinNum * 2U)) ; //skinyli vse chto tam stoit 
+    value|= (GPIOModule::MODER::FieldValues::Alternate::Value << (pinNum * 2U)) ; //setup new znachenie
+    GPIOModule::MODER::Write(value); //comeback 
+  }
+  
+  void SetInput()
+  {
+    volatile auto value = GPIOModule::MODER::Get() ; //chitivaem znachenia moder
+    value &= ~(3 << (pinNum * 2U)) ; //skinyli vse chto tam stoit 
+    value |= (GPIOModule::MODER::FieldValues::Input::Value << (pinNum * 2U)) ; //setup new znachenie
+    GPIOModule::MODER::Write(value); //comeback 
+  }
+  bool IsSet()
+  {
+    return !GPIOModule::IDR::IsSet();
+  }
+};
